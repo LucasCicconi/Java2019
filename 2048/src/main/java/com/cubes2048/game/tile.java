c14867e0172d0488eb488d6febc7ecf34d6b531e
derekfq <50533501+derekfq@users.noreply.github.com>
2019-09-12 11:03:41 -0300

    Add files via upload

---
+++ 2048/src/main/java/com/cubes2048/game/tile.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cubes2048.game;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author 18706986
+ */
+public class tile {
+    //public porque a classe do tabuleiro precisa saber essas variaveis
+    public static final int WIDTH = 80;   
+    public static final int HEIGHT = 80;
+    public static final int SLIDE_SPEED = 20;
+   
+    public static final int ARC_WIDTH=15;
+    public static final int ARC_HEIGHT=15;
+    
+    private int value;
+    private BufferedImage tileImage;
+    private Color background;
+    private Color text;
+    private Font font;
+    tileImage = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_ARGB);
+    
+
+    private int x;
+    private int y;
+    
+    public tile(int value,int x, int y)
+    {
+        this.value = value;
+        this.x = x;
+        this.y = y;
+        //vai desenhar no tile da imagem o plano de fundo e um numero
+        drawImage();
+    }
+    private void drawImage()
+    {
+        Graphics2D g = (Graphics2D) image.getGraphics();
+        
+    }
+    
+    
+}
