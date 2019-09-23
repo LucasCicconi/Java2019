e963396641a591da67ed350c82a97eff77e36133
derekfq <50533501+derekfq@users.noreply.github.com>
2019-09-23 09:21:14 -0300

    Update auxiliarImpressao.java

--- 2048/src/main/java/com/cubes2048/game/auxiliarImpressao.java
+++ 2048/src/main/java/com/cubes2048/game/auxiliarImpressao.java
@@ -10,5 +10,21 @@ package com.cubes2048.game;
  * @author 18706986
  */
 public class auxiliarImpressao {
+ 
+    private auxiliarImpressao(){};
     
-}
+    //Para centralizar as coisas no quadradin
+    public static int getMessageWidth(String message,Font font,Graphics2D g)
+    {
+   g.setFont(font);
+   Rectangle2D bounds = g.getFontMetrics().getStringBounds(message,g);
+   return(int)bounds.getWidth();
+    }
+    //
+    public static int getMessageHeight(String message,Font font,Graphics2D g)
+    {
+   g.setFont(font);
+   if(message.length()==0) return 0;
+   TextLayout tl = new TextLayout(message,font,g.getFontRenderContext());
+   return(int)tl.getBounds().getHeight();
+    }
