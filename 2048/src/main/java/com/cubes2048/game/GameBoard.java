3aec353847e86568313dfd42ca42765182a71fbe
derekfq <50533501+derekfq@users.noreply.github.com>
2019-09-26 18:43:19 -0300

    Update GameBoard.java

--- 2048/src/main/java/com/cubes2048/game/GameBoard.java
+++ 2048/src/main/java/com/cubes2048/game/GameBoard.java
@@ -22,8 +22,8 @@ public class GameBoard {
     
     private final int startingTiles = 2;
     private tile[][] board;
-    private boolean dead;
-    private boolean won;
+    private boolean perdeu;
+    private boolean ganhou;
     private BufferedImage gameBoard;
     private BufferedImage finalBoard;
     private int x;
@@ -74,8 +74,9 @@ public class GameBoard {
         {
             spawnRandom();
         }
-        
-    }   
+    }    
+
+    
     private void spawnRandom()
     {
      Random random = new Random();
@@ -127,7 +128,7 @@ public class GameBoard {
             
         }
         
-        g.drawImage(finalBoard,x,300,null);
+        g.drawImage(finalBoard,x,250,null);
         g2d.dispose();
         
     }
@@ -142,13 +143,273 @@ public class GameBoard {
                 tile current= board[row][col];
                 if(current == null)continue;
                 current.update();
-                //reseta a posiçao
+                resetPosition(current,row,col);
                 if(current.getValue() == 2048)
                 {
-                won = true;
+                ganhou = true;
                 }
             }
         }
+    }
+    
+    private void resetPosition(tile current, int row, int col)
+    {
+        if(current == null)return;
+        
+        int x = getTileX(col);
+        int y = getTileY(row);
+        
+        int distX = current.getX() - x;
+        int distY = current.getY() - y;
+        
+        if(Math.abs(distX)<tile.SLIDE_SPEED)
+        {
+            current.setX(current.getX()- distX);
+        }
+        
+        if(Math.abs(distY)<tile.SLIDE_SPEED)
+        {
+            current.setY(current.getY()-distY);
+        }
+        
+        if(distX < 0)
+        {
+        current.setX(current.getX() + tile.SLIDE_SPEED);
+        }
+        
+        if(distY < 0)
+        {
+            current.setY(current.getY() + tile.SLIDE_SPEED);
+        }
+        
+        if(distX > 0)
+        {
+            current.setX(current.getX() - tile.SLIDE_SPEED);
+        }
+        
+        if(distY > 0)
+        {
+            current.setY(current.getY() - tile.SLIDE_SPEED);
+        }
+        
+        
+    }
+    
+    
+    private boolean move(int row,int col,int direcaoHorizontal,int direcaoVertical,Direction dir)
+    {
+        boolean podeMover = false;
+        
+        tile current = board[row][col];
+        if(current == null)return false;
+        boolean move = true;
+        int newCol = col;
+        int newRow = row;
+        while(move)
+        {
+        newCol += direcaoHorizontal;
+        newRow += direcaoVertical;
+        if(checkOutBounds(dir,newRow,newCol))break;
+        if(board[newRow][newCol] == null)
+        {
+            board[newRow][newCol] = current;
+            board[newRow - direcaoVertical][newCol - direcaoHorizontal] = null;
+            board[newRow][newCol].setIrPara(new Point(newRow,newCol));
+            podeMover=true;
+        }
+        else if(board[newRow][newCol].getValue()==current.getValue()&& board[newRow][newCol].PodeCombinar())
+        {
+            board[newRow][newCol].setPodeCombinar(false);
+            board[newRow][newCol].setValue(board[newRow][newCol].getValue()*2);
+            podeMover = true;
+            board[newRow - direcaoVertical][newCol - direcaoHorizontal] = null;
+            board[newRow][newCol].setIrPara(new Point(newRow,newCol));
+           // board[newRow][newCol].setCombineAnimation(true);
+           //colocar score
+        }    
+        else
+        {
+            move =false;
+        }
+
+        }
+        
+        
+        
+    return podeMover;
+    }
+    
+    private boolean checkOutBounds(Direction dir, int row, int col) 
+    {
+        if(dir == Direction.LEFT)
+        {
+            return col < 0;
+        }
+        else if(dir == Direction.RIGHT)
+        {
+            return col > COLS -1;
+        }
+        else if(dir==Direction.UP)
+        {
+            return row < 0;
+        }
+        else if(dir == Direction.DOWN)
+        {
+            return row> ROWS-1;
+        }
+        return false;
+    }
+    
+    
+    private void moveTiles(Direction dir)
+    {
+        boolean podeMover = false;
+        int direcaoHorizontal = 0;
+        int direcaoVertical = 0;
+        
+        if(dir == Direction.LEFT)
+        {
+            direcaoHorizontal = -1;
+            for(int row= 0; row <ROWS;row++)
+            {
+                for(int col = 0;col<COLS;col++)
+                {
+                    if(!podeMover)
+                    {
+                        podeMover= move(row,col,direcaoHorizontal,direcaoVertical,dir);
+                
+                    }
+                    else move(row, col, direcaoHorizontal, direcaoVertical,dir);
+                }
+            }
+        }
+        
+        
+        else if(dir == Direction.RIGHT)
+        {
+            direcaoHorizontal = 1;
+             for(int row= 0; row <ROWS;row++)
+            {
+                for(int col = COLS - 1;col >= 0;col--)
+                {
+                     if(!podeMover)
+                    {
+                        podeMover= move(row,col,direcaoHorizontal,direcaoVertical,dir);
+                
+                    }
+                    else move(row,col,direcaoHorizontal,direcaoVertical,dir);
+                }
+            
+            }
+        }
+        
+        
+        else if(dir == Direction.UP)
+        {
+            direcaoVertical = -1;
+            for(int row= 0; row <ROWS;row++)
+            {
+                for(int col = 0;col<COLS;col++)
+                {
+                    if(!podeMover)
+                    {
+                        podeMover= move(row,col,direcaoHorizontal,direcaoVertical,dir);
+                
+                    }
+                    else move(row,col,direcaoHorizontal,direcaoVertical,dir);
+                }
+        
+            }
+        }
+        
+        else if(dir == Direction.DOWN)
+        {
+            direcaoVertical = 1;
+            for(int row= ROWS - 1; row >=0;row--)
+            {
+                for(int col = 0;col<COLS;col++)
+                {
+                    if(!podeMover)
+                    {
+                        podeMover= move(row,col,direcaoHorizontal,direcaoVertical,dir);
+                
+                    }
+                    else move(row,col,direcaoHorizontal,direcaoVertical,dir);
+                }
+            }
+        }
+        else
+        {
+            System.out.println(dir +" nao e uma direcao valida.");
+        }
+        
+        for(int row=0;row<ROWS;row++)
+        {
+            for(int col=0;col<COLS;col++)
+            {
+                tile current = board[row][col];
+                if(current == null)continue;
+                current.setPodeCombinar(true);
+                
+            }
+        }
+            if(podeMover)
+            {
+               spawnRandom();
+              //checar se perdeu
+              checarPerdeu();
+        
+            }
+            
+        }
+        
+        private void checarPerdeu()
+        {
+            for (int row = 0 ;row < ROWS;row++)
+            {
+                for (int col = 0; col<COLS;col++)
+                {
+                    if(board[row][col] == null)return;
+                    if(checarTilesEmVolta(row,col,board[row][col]))
+                    {
+                        return;
+                    }  
+                }
+            }
+            
+            perdeu = true;
+            //fzr o score
+        }
+        
+    private boolean checarTilesEmVolta(int row, int col, tile current)
+    {
+        if(row>0)
+        {
+             tile check = board[row-1][col];
+             if(check == null)return true;
+            if(current.getValue() == check.getValue())return true;
+        }
+        if(row<ROWS -1)
+        {
+            tile check =board[row+1][col];
+            if(check == null)return true;
+            if(current.getValue() == check.getValue())return true;
+        }
+        
+         if(col>0)
+        {
+            tile check =board[row][col - 1];
+            if(check == null)return true;
+            if(current.getValue() == check.getValue())return true;
+        }
+         
+          if(col<COLS -1)
+        {
+            tile check =board[row][col + 1];
+            if(check == null)return true;
+            if(current.getValue() == check.getValue())return true;
+        }
+        return false;
     }
     
     private void checkKeys()
@@ -156,24 +417,29 @@ public class GameBoard {
         if(teclado.typed(KeyEvent.VK_LEFT))
         {
         //manda os quadrados para a esquerda
+            moveTiles(Direction.LEFT);
             if(!hasStarted)hasStarted = true;
         }
         if(teclado.typed(KeyEvent.VK_RIGHT))
         {
         //manda os quadrados para a direita
+            moveTiles(Direction.RIGHT);
             if(!hasStarted)hasStarted = true;
         }
         if(teclado.typed(KeyEvent.VK_UP))
         {
         //manda os quadrados para cima
+            moveTiles(Direction.UP);
             if(!hasStarted)hasStarted = true;
         }
         if(teclado.typed(KeyEvent.VK_DOWN))
         {
         //manda os quadrados para baixo
+            moveTiles(Direction.DOWN);
             if(!hasStarted)hasStarted = true;
         }
         
     }
+
 }
 
