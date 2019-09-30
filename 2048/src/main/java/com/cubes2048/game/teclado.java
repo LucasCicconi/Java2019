678b7875e643d795c276bc9795333a952f800fc6
derekfq <50533501+derekfq@users.noreply.github.com>
2019-09-30 09:13:59 -0300

    implementaçao wasd

--- 2048/src/main/java/com/cubes2048/game/teclado.java
+++ 2048/src/main/java/com/cubes2048/game/teclado.java
@@ -21,12 +21,16 @@ public class teclado {
     }
     public static void update()
     {
-    for( int i=0;i<4;i++)
+    for( int i=0;i<8;i++)
         {
-         if(i==0)anterior[KeyEvent.VK_LEFT] =pressed[KeyEvent.VK_LEFT];
-         if(i==1)anterior[KeyEvent.VK_RIGHT] =pressed[KeyEvent.VK_RIGHT];
-         if(i==2)anterior[KeyEvent.VK_UP] =pressed[KeyEvent.VK_UP];
-         if(i==3)anterior[KeyEvent.VK_DOWN] =pressed[KeyEvent.VK_DOWN];
+         if(i==0)   anterior[KeyEvent.VK_LEFT] = pressed[KeyEvent.VK_LEFT];
+         if(i==1)   anterior[KeyEvent.VK_RIGHT] = pressed[KeyEvent.VK_RIGHT];
+         if(i==2)   anterior[KeyEvent.VK_UP] = pressed[KeyEvent.VK_UP];
+         if(i==3)   anterior[KeyEvent.VK_DOWN] = pressed[KeyEvent.VK_DOWN];
+         if(i==4)   anterior[KeyEvent.VK_A]  = pressed[KeyEvent.VK_A];
+         if(i==5)   anterior[KeyEvent.VK_D] = pressed[KeyEvent.VK_D];
+         if(i==6)   anterior[KeyEvent.VK_W]    = pressed[KeyEvent.VK_W];
+         if(i==7)   anterior[KeyEvent.VK_S]  = pressed[KeyEvent.VK_S];
         }
     }
     
